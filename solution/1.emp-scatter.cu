71e8173966a31cf559578910c5fbe221cfbc193c
LucaTosetti01 <luca1.tosetti@mail.polimi.it>
2024-08-31 18:30:16 +0200

    updated readme

--- solution/1.emp-scatter.cu
+++ solution/1.emp-scatter.cu
@@ -7,7 +7,8 @@
 #define CHECK(call)                                                                 \
   {                                                                                 \
     const cudaError_t err = call;                                                   \
-    if (err != cudaSuccess) {                                                       \
+    if (err != cudaSuccess)                                                         \
+    {                                                                               \
       printf("%s in %s at line %d\n", cudaGetErrorString(err), __FILE__, __LINE__); \
       exit(EXIT_FAILURE);                                                           \
     }                                                                               \
@@ -16,16 +17,17 @@
 #define CHECK_KERNELCALL()                                                          \
   {                                                                                 \
     const cudaError_t err = cudaGetLastError();                                     \
-    if (err != cudaSuccess) {                                                       \
+    if (err != cudaSuccess)                                                         \
+    {                                                                               \
       printf("%s in %s at line %d\n", cudaGetErrorString(err), __FILE__, __LINE__); \
       exit(EXIT_FAILURE);                                                           \
     }                                                                               \
   }
 
-#define GRID_DIM     128
-#define NUM_ATOMS    64
-#define ATOM_STRIDE  4   // Field in the atom data structure
-#define GRID_SPACING 0.5 //Angstrom
+#define GRID_DIM 128
+#define NUM_ATOMS 64
+#define ATOM_STRIDE 4    // Field in the atom data structure
+#define GRID_SPACING 0.5 // Angstrom
 
 #define MIN_COORD (GRID_SPACING * GRID_DIM)
 #define MAX_COORD (GRID_SPACING * GRID_DIM)
@@ -35,23 +37,28 @@
 
 #define BLOCK_DIM 128
 
-void cenergy(float *energygrid, const float *atoms) {
-  for (int t = 0; t < GRID_DIM; t++) {
-    const float z = GRID_SPACING * (float) t;
+void cenergy(float *energygrid, const float *atoms)
+{
+  for (int t = 0; t < GRID_DIM; t++)
+  {
+    const float z = GRID_SPACING * (float)t;
     // starting point of the slice in the energy grid
     const int grid_slice_offset = (GRID_DIM * GRID_DIM * z) / GRID_SPACING;
     // calculate potential contribution of each atom
-    for (int n = 0; n < NUM_ATOMS * ATOM_STRIDE; n += ATOM_STRIDE) {
-      const float dz     = z - atoms[n + 2]; // all grid points in a slice have the same
-      const float dz2    = dz * dz;          // z value, no recalculation in inner loops
+    for (int n = 0; n < NUM_ATOMS * ATOM_STRIDE; n += ATOM_STRIDE)
+    {
+      const float dz = z - atoms[n + 2]; // all grid points in a slice have the same
+      const float dz2 = dz * dz;         // z value, no recalculation in inner loops
       const float charge = atoms[n + 3];
-      for (int j = 0; j < GRID_DIM; j++) {
-        const float y             = GRID_SPACING * (float) j;
-        const float dy            = y - atoms[n + 1]; // all grid points in a row have the same
-        const float dy2           = dy * dy;          // y value
+      for (int j = 0; j < GRID_DIM; j++)
+      {
+        const float y = GRID_SPACING * (float)j;
+        const float dy = y - atoms[n + 1]; // all grid points in a row have the same
+        const float dy2 = dy * dy;         // y value
         const int grid_row_offset = grid_slice_offset + GRID_DIM * j;
-        for (int i = 0; i < GRID_DIM; i++) {
-          const float x  = GRID_SPACING * (float) i;
+        for (int i = 0; i < GRID_DIM; i++)
+        {
+          const float x = GRID_SPACING * (float)i;
           const float dx = x - atoms[n];
           energygrid[grid_row_offset + i] += charge / sqrtf(dx * dx + dy2 + dz2);
         }
@@ -62,26 +69,30 @@ void cenergy(float *energygrid, const float *atoms) {
 
 __constant__ float d_atoms[NUM_ATOMS * ATOM_STRIDE];
 
-void __global__ cenergy_gpu(float *energygrid) {
-  const int n        = (blockIdx.x * blockDim.x + threadIdx.x) * ATOM_STRIDE;
+void __global__ cenergy_gpu(float *energygrid)
+{
+  const int n = (blockIdx.x * blockDim.x + threadIdx.x) * ATOM_STRIDE;
   const float charge = d_atoms[n + 3];
   const float atom_x = d_atoms[n];
-  const float atom_y = d_atoms[n];
-  const float atom_z = d_atoms[n];
-  for (int t = 0; t < GRID_DIM; t++) {
-    const float z   = GRID_SPACING * (float) t;
-    const float dz  = z - atom_z; // all grid points in a slice have the same
-    const float dz2 = dz * dz;    // z value
+  const float atom_y = d_atoms[n + 1];
+  const float atom_z = d_atoms[n + 2];
+  for (int t = 0; t < GRID_DIM; t++)
+  {
+    const float z = GRID_SPACING * (float)t;
+    const float dz = z - atom_z; // all grid points in a slice have the same
+    const float dz2 = dz * dz;   // z value
     // starting position of the slice in the energy grid
     const int grid_slice_offset = (GRID_DIM * GRID_DIM * z) / GRID_SPACING;
-    for (int j = 0; j < GRID_DIM; j++) {
-      const float y   = GRID_SPACING * (float) j;
-      const float dy  = y - atom_y; // all grid points in a row have the same
-      const float dy2 = dy * dy;    // y value
+    for (int j = 0; j < GRID_DIM; j++)
+    {
+      const float y = GRID_SPACING * (float)j;
+      const float dy = y - atom_y; // all grid points in a row have the same
+      const float dy2 = dy * dy;   // y value
       // starting position of the row in the energy grid
       const int grid_row_offset = grid_slice_offset + GRID_DIM * j;
-      for (int i = 0; i < GRID_DIM; i++) {
-        const float x  = GRID_SPACING * (float) i;
+      for (int i = 0; i < GRID_DIM; i++)
+      {
+        const float x = GRID_SPACING * (float)i;
         const float dx = x - atom_x;
         atomicAdd(&energygrid[grid_row_offset + i], charge / sqrtf(dx * dx + dy2 + dz2));
       }
@@ -90,11 +101,13 @@ void __global__ cenergy_gpu(float *energygrid) {
   }
 }
 
-int main() {
+int main()
+{
   std::vector<float> atoms(NUM_ATOMS * ATOM_STRIDE);
   std::vector<float> energygrid(GRID_DIM * GRID_DIM * GRID_DIM, 0.f);
 
-  for (int i = 0; i < NUM_ATOMS * ATOM_STRIDE; i += ATOM_STRIDE) {
+  for (int i = 0; i < NUM_ATOMS * ATOM_STRIDE; i += ATOM_STRIDE)
+  {
     atoms[i + 0] =
         MIN_COORD + static_cast<float>(rand()) / (static_cast<float>(RAND_MAX / (MAX_COORD - MIN_COORD)));
     atoms[i + 1] =
@@ -125,11 +138,15 @@ int main() {
                    sizeof(float) * GRID_DIM * GRID_DIM * GRID_DIM,
                    cudaMemcpyDeviceToHost));
 
-  for (int t = 0; t < GRID_DIM; t++) {
-    for (int j = 0; j < GRID_DIM; j++) {
-      for (int i = 0; i < GRID_DIM; i++) {
+  for (int t = 0; t < GRID_DIM; t++)
+  {
+    for (int j = 0; j < GRID_DIM; j++)
+    {
+      for (int i = 0; i < GRID_DIM; i++)
+      {
         if (std::abs(energygrid[GRID_DIM * GRID_DIM * t + GRID_DIM * j + i] -
-                     h_energygrid[GRID_DIM * GRID_DIM * t + GRID_DIM * j + i]) > 1e-3) {
+                     h_energygrid[GRID_DIM * GRID_DIM * t + GRID_DIM * j + i]) > 1e-3)
+        {
           std::cout << "Electrostatic Map Potential CPU and GPU are NOT equivalent!" << std::endl;
           return EXIT_FAILURE;
         }
